2b75c40ba7f9fdc889ba210d8e33b3d82574be72
arthur790 <arturo.horse90@gmail.com>
2025-03-26 18:15:04 -0600

    configure users in guardian

--- lib/users_backend/guardian.ex
+++ lib/users_backend/guardian.ex
@@ -20,7 +20,7 @@ defmodule UsersBackend.Guardian do
     # Here we'll look up our resource from the claims, the subject can be
     # found in the `"sub"` key. In above `subject_for_token/2` we returned
     # the resource id so here we'll rely on that to look it up.
-    case ElixirApiJwt.Accounts.get_account!(id) do
+    case UsersBackend.Users.get_user!(id) do
       nil -> {:error, :reason_for_error}
       resource -> {:ok, resource}
     end
